ff127023bd2214a4095cde291e31311b3dd6e214
isaacmabueno <isaacmabueno@gmail.com>
2018-11-10 13:46:44 -0600

    removing code and making quick update to account app functionality

--- objects/object-references.js
+++ objects/object-references.js
@@ -1,5 +1,5 @@
 let myAccount = {
-    name: 'Isaac Bueno',
+    name: 'Isaac B',
     expenses: 0,
     income: 0
 }
@@ -8,24 +8,23 @@ let addIncome = function(amount, income) {
     amount.income = amount.income + income;
 }
 
-let addExpense = function(amount, expense) {
-    amount.expenses = amount.expenses + expense;
+let addExpense = function(amount, expenses) {
+    amount.expenses = amount.expenses + expenses;
 }
 
-let resetAccount = function(account) {
-    account.expenses = 0;
-    account.income = 0;
+let resetAccount = function(amount) {
+    amount.income = 0;
+    amount.expenses = 0;
 }
 
-let getAccountSummary = function(account) {
-    let balance = account.income - account.expenses;
-    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`;
+let getAccountSummary = function(amount) {
+    let accountBalance = amount.income - amount.expenses;
+    return `As of ${new Date()}, you have a balance of $${accountBalance}. Your income is $${amount.income}. Your expenses are $${amount.expenses}.`;
 }
 
-addIncome(myAccount, 1900);
-console.log(myAccount);
-addExpense(myAccount, 1400);
-console.log(myAccount);
+addIncome(myAccount, 2800);
+addExpense(myAccount, 2400);
+
 console.log(getAccountSummary(myAccount));
 resetAccount(myAccount);
-console.log(getAccountSummary(myAccount));
+console.log(myAccount)
