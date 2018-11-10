af14e33cdbe9feb5f9a872fe2f70f03418695acd
isaacmabueno <isaacmabueno@gmail.com>
2018-11-10 11:36:50 -0600

    creating a simple income calc

--- objects/object-references.js
+++ objects/object-references.js
@@ -1,54 +1,31 @@
-myAccount = {
-    name: 'Isaac B',
+let myAccount = {
+    name: 'Isaac Bueno',
     expenses: 0,
-    income: 0 
+    income: 0
 }
 
-let addExpense = function(account, amount) {
-    account.expenses = account.expenses + amount
-    console.log(account)
+let addIncome = function(amount, income) {
+    amount.income = amount.income + income;
 }
 
-
-let addIncome = function(account, amount) {
-    account.income = account.income + amount
-    console.log(account)
+let addExpense = function(amount, expense) {
+    amount.expenses = amount.expenses + expense;
 }
 
 let resetAccount = function(account) {
     account.expenses = 0;
     account.income = 0;
-    console.log(account)
 }
 
 let getAccountSummary = function(account) {
-    let balance = account.income - account.expenses
-    return `Account for ${account.name} has $${balance}. ${account.income} in income. ${account.expenses} in expenses.`
+    let balance = account.income - account.expenses;
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`;
 }
 
-addExpense(myAccount, 4300.00)
-addIncome(myAccount, 5600.00)
-
-console.log(getAccountSummary(myAccount))
-// resetAccount(myAccount)
-// let getAccountSummary = function(printBalance) {
-//     return `your account balance`
-//     console.log(printBalance)
-// } 
-
-// resetAccount - this reset the expenses and income to zero - just needs to know wht account to change
-
-//getAccountSummary - this will say
-// Account or Isaac has $900. $1000 in income. $100 in expense. console.log this
-
-
-// addIncome - done
-// addExpense
-// getAccountSummary
-// resetAccount
-
-// getAccountSummary
-// addExpense(myAccount, 4300.00)
-// addIncome(myAccount, 5600.00)
-// getAccountSummary(myAccount)
-// resetAccount(myAccount)
+addIncome(myAccount, 1900);
+console.log(myAccount);
+addExpense(myAccount, 1400);
+console.log(myAccount);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
