3dd23ebf8244f3b7adedb784d8a5392ad24ce65f
isaacmabueno <isaacmabueno@gmail.com>
2018-12-05 10:22:42 -0600

    updating number methods

---
+++ objects/number-methods.js
@@ -0,0 +1,12 @@
+// let num = 14.123
+
+// console.log(num.toFixed(2))
+// console.log(Math.round(num));
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+
+let min = 0
+let max = 1
+let random = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(random)
